0a42b3999f16dfa6d91f7be1a9ed62ae1fa4427b
yousef <youseff2000q@gmail.com>
2022-12-07 10:14:31 -0800

    edit

--- src/components/Dashbard/smallCompForDash/tickets/Tickets.js
+++ src/components/Dashbard/smallCompForDash/tickets/Tickets.js
@@ -2,35 +2,35 @@ import React, { useEffect } from "react";
 import Title from "../Title/Title";
 
 export default function Tickets() {
-  // useEffect(() => {
-  //   const ticket = document.querySelector(".tickets");
-  //   const TheNumbers = document.querySelectorAll(".tickets section >div >h3");
-  //   let status = true;
-  //   window.addEventListener("scroll", function () {
-  //     if (
-  //       window.scrollY >=
-  //       ticket.offsetTop + ticket.offsetHeight - window.innerHeight
-  //     ) {
-  //       if (status === true) {
-  //         TheNumbers.forEach((span) => {
-  //           let goal = span.dataset.point;
-  //           console.log(goal);
-  //           span.innerHTML = 0;
-  //           let counter = this.setInterval(() => {
-  //             span.textContent++;
-  //             console.log(span.textContent);
-  //             if (span.textContent == goal) {
-  //               this.clearInterval(counter);
-  //             }
-  //           }, 10);
-  //         });
-  //         status = !status;
-  //       }
-  //     } else {
-  //       console.log("object");
-  //     }
-  //   });
-  // }, []);
+  useEffect(() => {
+    const ticket = document.querySelector(".tickets");
+    const TheNumbers = document.querySelectorAll(".tickets section >div >h3");
+    let status = true;
+    window.addEventListener("scroll", function () {
+      if (
+        window.scrollY >=
+        ticket.offsetTop + ticket.offsetHeight - window.innerHeight
+      ) {
+        if (status === true) {
+          TheNumbers.forEach((span) => {
+            let goal = span.dataset.point;
+            console.log(goal);
+            span.innerHTML = 0;
+            let counter = this.setInterval(() => {
+              span.textContent++;
+              console.log(span.textContent);
+              if (span.textContent == goal) {
+                this.clearInterval(counter);
+              }
+            }, 10);
+          });
+          status = !status;
+        }
+      } else {
+        console.log("object");
+      }
+    });
+  }, []);
   return (
     <div
       className="col-xl-6 col-xxl-4  h-auto my-3 wow animate__slideInRight"
