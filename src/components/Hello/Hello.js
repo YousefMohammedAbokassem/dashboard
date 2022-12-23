7e3c10d71b58114d2c75469774870086e4ac6e49
yousef <youseff2000q@gmail.com>
2022-12-23 15:00:53 -0800

    Plans component

--- src/components/Hello/Hello.js
+++ src/components/Hello/Hello.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./hello.css";
 export default function Hello() {
   return (
-    <div className="hello position-absolute w-100 h-100 d-flex align-items-center justify-content-center">
+    <div className="hello position-fixed w-100 h-100 d-flex align-items-center justify-content-center">
      Welcome
     </div>
   );
